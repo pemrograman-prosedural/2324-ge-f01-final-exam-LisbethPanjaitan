f4a79b6ab9621becafec10b4a96634dd01519dff
LisbethPanjaitan <lisbethpanjaitan20@gmail.com>
2024-05-15 13:51:24 +0700

    Pair17

--- libs/dorm.c
+++ libs/dorm.c
@@ -1,7 +1,51 @@
 #include "dorm.h"
 
-/**
- * @brief Define the complete function definition here. Be sure to enlist the prototype of each function
- * defined here in the corresponding header file.
- *
- */
+// menggunakan fungsi struct dorm
+struct dorm_t create_dorm(char *_name, unsigned short _capacity, enum gender_t _gender)
+{
+    struct dorm_t dorm; // membuat struct dorm yang sementara
+    memset(temp.name, '\0', sizeof(temp.name));   // karakter diinisialisasi = NULL
+    strcpy(temp.name, _name);  // parameter ke  struct dorm sementara
+    temp.capacity = _capacity;
+    temp.resident_num = 0;  // mengatur jumlah penghuni asrama = 0
+    temp.gender = _gender;  // menetapkan jenis kelamin dari parameter
+    return temp;
+}
+
+// cetak detail dorm
+void print_dorm(struct dorm_t *dorm, int temphsow)
+{
+    int j;
+    // buat loopng setiap dorm
+    for (j = 0; j < tempshow; j++)
+    {
+        printf("%s|%d|", dorm[j].name, dorm[j].capacity);
+        if (dorm[j].gender == GENDER_MALE) // Jika jenis kelamin dorm adalah pria
+        {
+            printf("male\n"); 
+        }
+        else // Jika jenis kelamin dorm adalah wanita
+        {
+            printf("female\n"); 
+        }
+    }
+}
+
+// mencetak detail lengkap dorm
+void print_dorm_detail(struct dorm_t *dorm, int tempshow)
+{
+    int j;
+    for (j = 0; j < tempshow; j++) // Looping melalui setiap dorm
+    {
+        printf("%s|%d|", dorm[j].name, dorm[j].capacity); // Mencetak nama dan kapasitas dorm
+        if (dorm[j].gender == GENDER_MALE) // Jika jenis kelamin dorm adalah pria
+        {
+            printf("male|"); // Mencetak "male"
+        }
+        else // Jika jenis kelamin dorm adalah wanita
+        {
+            printf("female|"); // Mencetak "female"
+        }
+        printf("%d\n", dorm[j].residents_num); // Mencetak jumlah penghuni dorm
+    }
+}
--- libs/dorm.h
+++ libs/dorm.h
@@ -2,6 +2,12 @@
 #define DORM_H
 
 #include "gender.h"
+#include "student.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
 
 /**
  * @brief define your structure, enums, globally accessible variables, and function prototypes here.
@@ -11,12 +17,17 @@
 
 struct dorm_t
 {
-  char name[20];
-  unsigned short capacity;
+  char name[20]; // nama dorm dengan panjang 20 karakter
+  unsigned short capacity; // kapasitas dorm diatur ke unsigned
   enum gender_t gender;
   unsigned short residents_num;
 };
 
+// membuat dorm baru
 struct dorm_t create_dorm(char *_name, unsigned short _capacity, enum gender_t _gender);
+// cetak detail dorm
+void print_dorm(struct dorm_t *dorm, int tempshow);
+// cetak detail lengkap dorm
+void print_dorm_detail(struct dorm_t *dorm, int tempshow);
 
 #endif
